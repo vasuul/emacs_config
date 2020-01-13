f2586afdb8238d12f3871d756acce50b0eed4597
Maxwell Walter <mwalter@iveia.com>
2020-01-13 18:57:47 -0500

    Beginning a new version of my config, almost from scratch.
    I didn't like how I set up my old config, so I am starting over.

--- custom.el
+++ custom.el
@@ -1,44 +1,16 @@
+;; System specific emacs stuff goes here
+
+;; Point to ispell for spell checking
+(setq ispell-program-name "/usr/local/bin/ispell")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(blink-cursor-mode nil)
- '(c-basic-offset 2)
- '(c-default-style "linux")
- '(column-number-mode t)
- '(custom-enabled-themes (quote (mytheme)))
- '(custom-safe-themes
-   (quote
-    ("33d50ff2a127ebc4ee179835bde48499589025cc6a7b164eda532042f1657732" "39ae2f2e25d35b785bf99b2d094fbdedaa36650b18b494a45e0189e3bd81de1f" default)))
- '(ecb-options-version "2.40")
- '(gdb-enable-debug t)
- '(gdb-many-windows t)
- '(gdb-speedbar-auto-raise t)
- '(indent-tabs-mode nil)
- '(inhibit-startup-screen t)
- '(make-backup-files nil)
- '(mouse-wheel-follow-mouse (quote t))
- '(mouse-wheel-progressive-speed t)
- '(mouse-wheel-scroll-amount (quote (1 ((shift) . 1))))
- '(package-selected-packages (quote (sr-speedbar ztree nyan-mode)))
- '(scroll-step 1)
- '(show-paren-mode t)
- '(speedbar-show-unknown-files t)
- '(speedbar-update-flag t)
- '(sr-speedbar-auto-refresh t)
- '(sr-speedbar-right-side nil)
- '(sr-speedbar-skip-other-window-p t)
- '(standard-indent 2)
- '(tool-bar-mode nil)
- '(toolbar-visible-p nil))
-
+ '(package-selected-packages (quote (nyan-mode esup use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :height 110 :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal))))
- '(minimap-font-face ((t (:height 20 :family "DejaVu Sans Mono")))))
+ )
--- init.el
+++ init.el
@@ -1,9 +1,16 @@
 ;; Turn off mouse interface early in startup to avoid momentary display
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;; No bells
+(setq ring-bell-function 'ignore)
 
 ;; No splash screen
 (setq inhibit-startup-message t)
+
+;; Stretch the cursor over multi char glyphs (i.e. tabs)
+(setq x-stretch-cursor t)
 
 ;; Set the default window size
 (add-to-list 'default-frame-alist '(height . 60))
@@ -19,28 +26,111 @@
 (package-initialize)
 
 ;; Set up our package locations
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+;; ("melpa-stable" . "https://stable.melpa.org/packages/")
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("gnu" . "https://elpa.gnu.org/packages/")))
 
-;; Set up load path
-(add-to-list 'load-path settings-dir)
-(add-to-list 'load-path site-lisp-dir)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Taken from config at
+;; https://github.com/nilsdeppe/MyEnvironment/blob/master/.emacs.el
+(mapc #'(lambda (add) (add-to-list 'load-path add))
+      (eval-when-compile
+        (require 'package)
+        (package-initialize)
+        ;; Install use-package if not installed yet.
+        (unless (package-installed-p 'use-package)
+          (package-refresh-contents)
+          (package-install 'use-package))
+        ;; (require 'use-package)
+        (let ((package-user-dir-real (file-truename package-user-dir)))
+          ;; The reverse is necessary, because outside we mapc
+          ;; add-to-list element-by-element, which reverses.
+          (nreverse
+           (apply #'nconc
+                  ;; Only keep package.el provided loadpaths.
+                  (mapcar #'(lambda (path)
+                              (if (string-prefix-p package-user-dir-real path)
+                                  (list path)
+                                nil))
+                          load-path))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Point to ispell for spell checking
-(setq ispell-program-name "/usr/local/bin/ispell")
+;; Change the default garbage collection threshold during startup
+;; Then put it back to something small after we start so that we areen't
+;;  garbage collecting so much during startup
+(setq gc-cons-threshold 64000000)
+(add-hook 'after-init-hook #'(lambda ()
+			       (setq gc-cons-threshold 1000000)))
+
+(show-paren-mode t)               ; Highlight matching braces
+(delete-selection-mode t)         ; overwrite the selected section on edit
+(setq column-number-mode t)       ; Show column numbers
+(setq make-backup-files nil)      ; don't make backup files
+(setq-default case-fold-search t  ; Case insensitive search by default
+	      search-highlight t) ; highlight all matches
+(global-hl-line-mode t)           ; Highlight the current line
+
+;; Delete trailing whitespace on save
+(add-hook 'before-save-hook
+	  (lambda ()
+	    delete-trailing-whitespace))
+
+;; Don't ask to follow version-control symlinks.  Just do it
+(setq vc-follow-symlinks t)
+
+(eval-when-compile
+  (require 'use-package))
+(use-package use-package
+  :commands use-package-autoload-keymap)
 
 ;; Keep emacs Custom-settings in separate file
+;; Ideally this will be where the OS/system specific stuff goes
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
+(if (file-exists-p custom-file)
+    (load custom-file))
+
+;; And now...
+;;  We load all of our packages
+;; TODO: Maybe put this into a packages.el file?
+
+;; Emacs starup profiler
+(use-package esup
+  :ensure t
+  :init
+  (setq esup-child-max-depth 0)
+  ;; Use a hook so the message doesn't get clobbered by other messages.
+  (add-hook
+   'emacs-startup-hook
+   (lambda ()
+     (message "Emacs took %s to start with %d garbage collections."
+              (format "%.2f seconds"
+                      (float-time
+                       (time-subtract after-init-time before-init-time)))
+              gcs-done))))
+
+;; nyan mode because I like it...
+(use-package nyan-mode
+  :ensure t
+  :config
+  (setq nyan-wavy-trail t)
+  (setq nyan-bar-length 30)
+  (setq nyan-minimum-window-width 80)
+  (nyan-mode))
+
+
+;(setq rainbow-file (expand-file-name "rainbow-mode-1.0.1.el" user-emacs-directory))
+;(if (file-exists-p rainbow-file)
+;    ((load rainbow-file)
+;     (require 'rainbow-mode)))
 
 ;; Load nyan mode becuase it is fun
-(require 'nyan-mode)
-(setq nyan-wavy-trail t)
-(nyan-mode)
+;(require 'nyan-mode)
+;(setq nyan-wavy-trail t)
+;(nyan-mode)
 
 ;; Set speed bar do special-d
-(global-set-key (kbd "s-d") 'sr-speedbar-toggle)
+;(global-set-key (kbd "s-d") 'sr-speedbar-toggle)
 
 ;; Finally, load my special fancy modeline
-(load (expand-file-name "modeline.el" user-emacs-directory))
+;(load (expand-file-name "modeline.el" user-emacs-directory))
 
